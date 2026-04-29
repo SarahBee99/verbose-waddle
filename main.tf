da951ad761f31af0f7e0c0a8596c6935f19b4cda
Sarah Rashed <sarahbelal.99.1a19@gmail.com>
2026-04-29 17:32:29 +0300

    Added egress rule

--- main.tf
+++ main.tf
@@ -28,6 +28,18 @@ resource "aws_security_group" "server_sg" {
   }
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow_outboundtraffic" {
+  security_group_id = aws_security_group.server_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 0
+
+  tags = {
+    Name      = "For all outboundtraffic"
+  }
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.server_sg.id
   cidr_ipv4         = "0.0.0.0/0"
